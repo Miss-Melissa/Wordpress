c6dea7b07b20ed18dd1739196543cc74e385690c
Miss-Melissa <94617701+Miss-Melissa@users.noreply.github.com>
2022-09-12 23:30:24 +0200

    fixes in code

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php wp_footer(); ?>
 
 <div class="wrapper">
-    <?= get_template_part('template-parts/postfooter'); ?>
+    <?= get_template_part('template-parts/post-footer'); ?>
 </div>
 
 </body>
